0d180c4d50b6f897d231d50d814f9437d96078d9
承续缘Win <2547393607@qq.com>
2021-12-06 20:53:03 +0800

    调整目录结构，小模块放入Epitome.Utility中

---
+++ Assets/Epitome/Epitome.Utility/Epitome.Network/NewHTTP.cs
@@ -0,0 +1,103 @@
+﻿/*----------------------------------------------------------------
+ * 文件名：NewHTTP
+ * 文件功能描述：超文本传输协议
+----------------------------------------------------------------*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Epitome.Network
+{
+    //===============命名空间=========================
+    using Manager;
+    //===============命名空间=========================
+
+    public class NewHTTP : MonoBehaviour
+	{
+		static NewHTTP mInstance;
+		public static NewHTTP GetSingleton()
+		{
+			if(mInstance==null)
+			{
+				//尝试寻找该类的实例。此处不能用GameObject.Find，因为MonoBehaviour继承自Component。
+				mInstance = Object.FindObjectOfType(typeof(NewHTTP)) as NewHTTP;
+
+				if (mInstance == null)
+				{                                       
+					GameObject tempGame = new GameObject("NewHTTP");
+					//防止被销毁
+					DontDestroyOnLoad(tempGame);
+					mInstance = tempGame.AddComponent<NewHTTP>();
+				}
+			}
+			return mInstance;
+		}
+
+        //++++++++++++++++++++     信息输入输出     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
+        /// <summary>
+        /// HTTP请求输入数据.
+        /// </summary>
+        public void HTTP_Request(object varObj)
+		{
+            StartCoroutine(WWWConnectionServer (varObj));
+		}
+			
+		/// <summary>
+		/// HTTP响应返回数据.
+		/// </summary>
+		void HTTP_Respond(string varEvent, WWW varWWW)
+		{
+			EventManager.Instance.BroadcastEvent (varEvent, varWWW as object);
+		}
+
+        //++++++++++++++++++++     连接服务器     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /// <summary>
+        /// HTTP连接服务器.
+        /// </summary>
+        IEnumerator WWWConnectionServer(object varObj)
+		{
+            List<object> tempObj = varObj as List<object>;
+
+            WWW tempWWW;
+
+            if (tempObj[1].ToString() == HTTPType.HTTP_Url.ToString())
+                tempWWW = new WWW(tempObj[2].ToString());
+            if (tempObj[1].ToString() == HTTPType.HTTP_byte.ToString())
+                tempWWW = new WWW(tempObj[2].ToString(), tempObj[3] as byte[]);
+            else if (tempObj[1].ToString() == HTTPType.HTTP_WWWForm.ToString())
+                tempWWW = new WWW(tempObj[2].ToString(), tempObj[3] as WWWForm);
+            else if (tempObj[1].ToString() == HTTPType.HTTP_Dictionary.ToString())
+                tempWWW = new WWW(tempObj[2].ToString(), tempObj[3] as byte[], tempObj[4] as Dictionary<string, string>);
+            else
+                tempWWW = new WWW("");
+
+            yield return tempWWW;
+			HTTP_Respond (tempObj[0].ToString(), tempWWW);
+		}
+	}
+
+    /// <summary>
+	/// HTTP类型.
+	/// </summary>
+	public enum HTTPType
+    {
+        /// <summary>
+        /// HTTP 参数（string varUrl）.
+        /// </summary>
+        HTTP_Url,
+        /// <summary>
+        /// HTTP 参数（string varUrl,WWWForm varWWWForm）.
+        /// </summary>
+        HTTP_WWWForm,
+        /// <summary>
+        /// HTTP 参数（string varUrl,byte[] varByte）.
+        /// </summary>
+        HTTP_byte,
+        /// <summary>
+        /// HTTP 参数（string varUrl,byte[] varByte,Dictionary<string,string> varDictionary）.
+        /// </summary>
+        HTTP_Dictionary,
+    }
+}
---
+++ Assets/Epitome/Epitome.Utility/Epitome.Utility.Audio/HelpTool/AudioHelpTool.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Epitome.Utility;
+using System.IO;
+using Epitome;
+using System.Threading;
+using System;
+using OfficeOpenXml;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Epitome.Audio
+{
+    public static class AudioHelpTool
+    {
+#if UNITY_EDITOR
+        /// <summary>
+        /// MP3文件转WAV文件
+        /// </summary>
+        [MenuItem("Epitome/Audio/MP3TurnWAV")]
+        [MenuItem("Assets/Epitome/Audio/MP3TurnWAV")]
+        private static void MP3TurnWAV()
+        {
+            List<string> paths = EditorExtension.GetSelectionPath();
+
+            string rootPath = ProjectPath.GetDataPath.Substring(0, ProjectPath.GetDataPath.Length - 6);
+
+            for (int i = 0; i < paths.Count; i++)
+            {
+                if (Directory.Exists(paths[i]))
+                {
+                    DirectoryInfo info = new DirectoryInfo(rootPath + paths[i]);
+
+                    string savePath = info.FullName + "_WAV";
+
+                    Project.CreateDirectory(savePath);
+
+                    Loom.RunAsync(() =>
+                    {
+                        List<string> strs = Project.DirectoryAllFileNames(info.FullName, new List<string>() { "mp3" });
+
+                        AudioConverter.MP3TurnWAV(info.FullName, savePath, strs);
+                    });
+                }
+                else if (File.Exists(paths[i]))
+                {
+                    FileInfo info = new FileInfo(rootPath + paths[i]);
+                    string[] fileName = info.Name.Split('.');
+
+                    if (fileName[fileName.Length - 1] == "mp3")
+                    {
+                        string savePath = string.Format("{0}/{1}.wav", info.Directory.FullName, fileName[0]);
+
+                        AudioConverter.MP3TurnWAV(info.FullName, savePath);
+                        AssetDatabase.Refresh();
+                    }
+                }
+            }
+        }
+
+        private static Task ReadAudio;
+
+        /// <summary>
+        /// 生成音频表格
+        /// </summary>
+        [MenuItem("Epitome/Audio/GenerateAudioTable")]
+        [MenuItem("Assets/Epitome/Audio/GenerateAudioTable")]
+        private static void GenerateAudioTable()
+        {
+            List<string> paths = EditorExtension.GetSelectionPath();
+
+            string rootPath = ProjectPath.GetDataPath.Substring(0, ProjectPath.GetDataPath.Length - 6);
+
+
+            if (paths.Count > 0 && Directory.Exists(paths[0]))
+            {
+                DirectoryInfo info = new DirectoryInfo(rootPath + paths[0]);
+
+                List<string> strs = Project.DirectoryAllFileFullName(info.FullName, new List<string>() { "wav", "aif", "ogg" });
+                Debug.Log(strs.Count);
+                if (ReadAudio != null)
+                {
+                    ReadAudio.Stop();
+                    ReadAudio = null;
+                }
+                ReadAudio = new Task(AudioLoad.Load(strs.ToArray(), AudioProcessing));
+            }
+        }
+
+        private static void AudioProcessing(List<AudioClip> clips, List<string> audioPaths)
+        {
+            List<string> header = new List<string>();
+            header.Add("MusicName");
+            header.Add("Duration");
+            header.Add("Path");
+
+            List<List<string>> tableData = new List<List<string>>();
+            for (int i = 0; i < clips.Count; i++)
+            {
+                int minute = (int)clips[i].length / 60;
+                int seconds = (int)clips[i].length % 60;
+
+                List<string> rowData = new List<string>();
+                rowData.Add(Path.GetFileNameWithoutExtension(audioPaths[i]));
+                rowData.Add(string.Format("{0:D2}:{1:D2}", minute, seconds));
+
+                string path = audioPaths[i].Substring(audioPaths[i].IndexOf("Assets"));
+                path = path.Replace('\\', '/');
+
+                rowData.Add(path);
+
+                tableData.Add(rowData);
+            }
+
+            FileInfo info = new FileInfo(audioPaths[0]);
+
+            string filePath = info.Directory.Parent.FullName + "/" + info.Directory.Name + ".xlsx";
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                using (ExcelPackage package = new ExcelPackage(stream))
+                {
+                    WriterExcel.NewWorkSheet(package, "Music", header, tableData);
+                }
+            }
+
+            if (ReadAudio != null)
+            {
+                ReadAudio.Stop();
+                ReadAudio = null;
+            }
+        }
+#endif
+    }
+}
